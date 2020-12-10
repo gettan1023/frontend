99ce41967bce961b88cf99bf27a26e6b92369a26
hide <nkret805worm@gmail.com>
2020-12-10 22:49:01 +0900

    サンプル追加

--- src/containers/Top.tsx
+++ src/containers/Top.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import {Typography, Button, Select, Card} from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
 import Header from "components/molecules/Header";
 import RoomMake from "components/molecules/RoomMake";
+
+const StyledText = styled(Typography)({
+    color: "red"
+});
 
 const Top: React.FC = () => {
     return(
         <>
             <Header/>
             <RoomMake/>
-            <h1>Hello World</h1>
+            <StyledText>Hello World</StyledText>
         </>
     );   
 };
